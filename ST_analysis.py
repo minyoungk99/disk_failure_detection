3c88eb3bc3cda5469cf147b88958da754ea51e06
MInYoung <mkim954@gatech.edu>
2024-12-01 19:15:30 -0600

    Classifier cmparison

--- ST_analysis.py
+++ ST_analysis.py
@@ -11,7 +11,7 @@ from sklearn.manifold import Isomap
 import scikitplot as skplt
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
-
+from matplotlib.colors import ListedColormap
 
 '''
 def plot_by_date(X, nrow, ncol):
@@ -159,9 +159,8 @@ def scatter_class(X, y):
     plt.ylabel("Principal Component 4")
     plt.show()
 
-
-#kpca = KernelPCA(n_components=9, kernel='rbf')
-kpca = KernelPCA(n_components=2, kernel='rbf')
+n_components = 2
+kpca = KernelPCA(n_components=n_components, kernel='rbf')
 X_kpca_train = kpca.fit_transform(X_train)
 #print("Kernel PCA X_train data:", X_kpca_train.shape)
 
@@ -245,3 +244,52 @@ knn = KNeighborsClassifier(n_neighbors=4)
 knn.fit(X_kpca_train, y_train)
 knn_pred = knn.predict(X_kpca_test)
 model_metrics(y_test, knn_pred, "KNN")
+
+#visualize decision boundaries
+# referenced plot_classifier_comparison.py from module 9 demo code
+if n_components == 2:
+    classifiers = [gnb, logreg, knn]
+    names = ["Gaussian NB", "Logistic Regression", "KNN N=4"]
+
+    # define meshgrid
+    cm = plt.cm.RdBu
+    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+    h = 0.2
+    x_min, x_max = X_kpca_train[:, 0].min() - .5, X_kpca_train[:, 0].max() + .5
+    y_min, y_max = X_kpca_train[:, 1].min() - .5, X_kpca_train[:, 1].max() + .5
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+    figure = plt.figure(figsize=(13, 5))
+    for i in range(len(classifiers)):
+        name = names[i]
+        clf = classifiers[i]
+
+        ax = plt.subplot(1, len(classifiers), i + 1)
+        if hasattr(clf, "decision_function"):
+            Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+        else:
+            Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+
+        # Put the result into a color plot
+        Z = Z.reshape(xx.shape)
+        ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+
+        # Plot the training points - uncomment to plot training data points
+        ax.scatter(X_kpca_train[:, 0], X_kpca_train[:, 1], c=y_train, cmap=cm_bright, edgecolors='k', marker='.', alpha=0.5)
+        # Plot the testing points
+        #ax.scatter(X_kpca_test[:, 0], X_kpca_test[:, 1], c=y_test, cmap=cm_bright, edgecolors='k', alpha=0.6)
+
+        ax.set_xlim(xx.min(), xx.max())
+        ax.set_ylim(yy.min(), yy.max())
+        ax.set_xticks(())
+        ax.set_yticks(())
+        ax.set_title(name)
+        # ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
+        #        size=15, horizontalalignment='right')
+
+    plt.tight_layout()
+    plt.show()
+
+
+
+
